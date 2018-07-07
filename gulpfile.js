bd9fa5807799ccc8576d1d79372cb4a6f5c21660
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-07 02:54:09 +0200

    Init commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+
+gulp.task('default', ['serve']);
