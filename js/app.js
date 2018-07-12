4b4f09e3cdab344ddf1878c56249d50ccf6b6b33
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-12 23:06:46 +0200

    Register service worker

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Register service worker
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('sw.js').then(function(reg) {
+		console.log('Service worker registered successfully!');
+	}).catch(function(err) {
+		console.log('Sorry, there was a problem to register service worker');
+	});
+}
