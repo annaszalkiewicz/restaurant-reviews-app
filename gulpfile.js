cbc37bdaa522cf1b7f41d95ecd2313f0fdc5d41f
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-16 17:22:30 +0200

    Add babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,36 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    },
-  })
-  gulp.watch('css/*.css').on('change', browserSync.reload);
-  gulp.watch('*.html').on('change', browserSync.reload);
-  gulp.watch('js/*.js').on('change', browserSync.reload);
-})
+gulp.task('serve', () =>  {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		},
+	});
+	gulp.watch('css/*.css').on('change', browserSync.reload);
+	gulp.watch('*.html').on('change', browserSync.reload);
+	gulp.watch('js/*.js').on('change', browserSync.reload);
+});
 
-gulp.task('default', ['serve']);
+gulp.task('babel', () =>
+	gulp.src('js/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['env']
+		}))
+		// .pipe(concat('app.js'))
+		// .pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist/js'))
+);
+gulp.task('sw', () =>
+	gulp.src('sw.js')
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.pipe(gulp.dest('dist'))
+);
+
+gulp.task('default', ['serve', 'babel', 'sw']);
