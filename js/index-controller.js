c8862423457d7882d0daf30da65513834d1367c1
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-07-18 10:55:24 +0200

    Edit url to service worker

--- js/index-controller.js
+++ js/index-controller.js
@@ -1,7 +1,7 @@
 // Register service worker
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js').then(function(reg) {
+	navigator.serviceWorker.register('./sw.js').then(function(reg) {
 		console.log('Service worker registered successfully!');
 	}).catch(function(err) {
 		console.log('Sorry, there was a problem to register service worker');
